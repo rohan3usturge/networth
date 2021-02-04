20122d7191a947f9a881733f7f1e991e641a2d2a
Rohan Usturge <rohan.usturge@hotmail.com>
2021-02-04 03:05:03 -0500

    fixes

--- server/infra/src/main/java/com/networth/infra/money/InfraMoneyClient.java
+++
@@ -1,7 +0,0 @@
-package com.networth.infra.money;
-
-public interface InfraMoneyClient {
-
-    public InfraMoney convert(Double value, String currencyCode, String targetCurrencyCode);
-
-}
--- server/infra/src/test/java/com/networth/infra/InfraApplicationTests.java
+++ server/infra/src/test/java/com/networth/infra/InfraApplicationTests.java
@@ -1,8 +1,6 @@
 package com.networth.infra;
 
-import com.networth.infra.money.InfraMoney;
-import com.networth.infra.money.InfraMoneyClient;
-import com.networth.infra.money.JavaMoneyBasedMoneyClient;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +11,6 @@ class InfraApplicationTests {
 	@Test
 	void contextLoads() {
 
-		InfraMoneyClient moneyClient = new JavaMoneyBasedMoneyClient();
-		InfraMoney convert = moneyClient.convert(2.0, "CAD", "INR");
-		System.out.println(convert);
 	}
 
 }
