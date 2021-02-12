bc7bc9ab313729ba1f907ec294a051f2029cbd27
Rohan Usturge <rohan.usturge@hotmail.com>
2021-02-12 11:47:36 -0500

    ci: add nextjs preset back to babel

--- web-client/babel.config.js
+++ web-client/babel.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   env: {
+    production: {
+      presets: ["next/babel"],
+    },
     test: {
-      presets: [
-        "@babel/preset-env",
-        "@babel/react",
-        "next/babel",
-        "@babel/preset-react",
-      ],
+      presets: ["@babel/preset-env", "@babel/react", "next/babel"],
     },
     development: {
       presets: ["next/babel"],
